8394c3b7373070102ac3898adb0e77112a2dddd5
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2025-03-26 18:02:49 +0300

    feat: 🎸 Improve

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 func main() {
+	arr := [3]int{10, 20, 30}
+	p := unsafe.Pointer(&arr[0])
+	p2 := unsafe.Pointer(uintptr(p) + unsafe.Sizeof(arr[0]))
+	fmt.Println(*(*int)(p2))
 }
