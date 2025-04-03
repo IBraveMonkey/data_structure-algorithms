739d0a058d5972eca5986b0b93aaacedcbb7d4e6
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2025-04-03 16:39:03 +0300

    docs: ✏️ Docs
    ✅ Closes: git push origin

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
+
+	"github.com/IBraveMonkey/data-structure-algorithms/quick_sort"
+)
+
+const (
+	Sunday = iota
+	Monday
+	Tuesday
 )
 
 func main() {
-	arr := [3]int{10, 20, 30}
-	p := unsafe.Pointer(&arr[0])
-	p2 := unsafe.Pointer(uintptr(p) + unsafe.Sizeof(arr[0]))
-	fmt.Println(*(*int)(p2))
+	arr := []int{8, 0, 10, 4, 15, 2, 24}
+
+	fmt.Println(quick_sort.QuickSort2(arr))
+
+	// Работа с арифметикой указателей
+	// arr := [3]int{10, 20, 30}
+	// p := unsafe.Pointer(&arr[0])
+	// p2 := unsafe.Pointer(uintptr(p) + unsafe.Sizeof(arr[0]))
+	// fmt.Println(*(*int)(p2))
 }
